eb9ee22aecfe941ae7a4d2ec7ff468198e25b902
Owen Shi <2008owenshi@gmail.com>
2022-11-07 16:46:58 -0500

    RESOLVE

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr'],
   },
-  localePath: './langs',
+  localePath: path.resolve('./langs'),
 };
